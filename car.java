ca7aad62400f738814ade8bf94d71d3e7dce38eb
kubrae <130611542+kubrae@users.noreply.github.com>
2023-04-29 02:25:10 +0300

    Update car.java

--- car.java
+++ car.java
@@ -1,19 +1,6 @@
 // hari bazi kodlar yazdim
 //diger developer taskini tamamladi
 //yeni kodlar ekledim
-
-
-
 //yeni kodlarrrr farkli kodlar 
-
 //bazi kodlar yazdik
 //lokalde conflig olursa
-
-//asdfghjkl;''
-//asdfghjkl;'
-lkjhgvfcvb
-mnbvfcgyhujikol
-'lkjhbvgcvbnm,.,mn'
-
-// remotta yeni kod yazdim 
-
